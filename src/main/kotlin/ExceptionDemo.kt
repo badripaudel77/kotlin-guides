0841b1a80d553b81980299dae8878033b7123c11
badripaudel77 <badri@eventlogic.se>
2022-05-16 21:22:12 +0545

    custom exception added

--- src/main/kotlin/ExceptionDemo.kt
+++ src/main/kotlin/ExceptionDemo.kt
@@ -7,17 +7,27 @@
 fun main() {
     // throw Exception("Exception ...")
     //throw IllegalArgumentException("Illegal arg exp")
+
+    // throw ExceptionDemo("custom exception"), throwing custom exception defined below
     var result: Int = 0
     val num1: Int = 10
     val num2: Int = 0
 
     try {
         result = num1 / num2
+        println("Result is ${result}")
     }
     catch (exception: Exception) {
         println("Exception occurred, message is : ${exception.message}")
     }
     finally {
         println("Always runs !!! ")
+        throw ExceptionDemo("custom exception") // throw custom exception
     }
 }
+
+// just extend the Exception class
+
+class ExceptionDemo(exception: String) : Exception(exception)
+// class ExceptionDemo(exception: String) : RuntimeException(exception)
+
